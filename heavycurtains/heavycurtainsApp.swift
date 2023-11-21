65d6e6c021683c84001d7bc3673d6ce61ae70365
Chris Ubick <chrisubick@gmail.com>
2023-11-20 16:12:36 -0800

    Initial Commit

---
+++ heavycurtains/heavycurtainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  heavycurtainsApp.swift
+//  heavycurtains
+//
+//  Created by Chris Ubick on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct heavycurtainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
